f08f174b7bb2b653c22187406ee0a5e3f86df3f2
april sikes <aprilsikes@gmail.com>
2016-01-18 16:04:42 -0700

    development typo in knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,10 @@
 module.exports = {
 
-  develpment: {
+  development: {
     client: 'pg',
     connection: 'postgres://localhost/books-bugs'
   },
+
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL
